f80f9477ca6e76c06bcf4d2bc30188258df5c071
Derek Fujimoto <fujimoto@phas.ubc.ca>
2018-10-29 00:12:48 -0700

    init imports bdata helper objects

--- bdata/__init__.py
+++ bdata/__init__.py
@@ -1,4 +1,8 @@
 from bdata.bdata import bdata
+from bdata.bdata import bdict
+from bdata.bdata import bvar
+from bdata.bdata import bhist
+from bdata.bdata import bscaler
 from bdata import mudpy
 
 __all__ = ['bdata','mudpy']
