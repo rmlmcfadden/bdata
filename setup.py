399c2ace9a6b0090100345b7b6c983de51d2ee3b
Derek Fujimoto <fujimoto@phas.ubc.ca>
2019-09-05 11:33:17 -0700

    v4.2.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ ext = Extension("bdata.mudpy",
 
 setuptools.setup(
     name="bdata",
-    version="4.2.2",
+    version="4.2.3",
     author="Derek Fujimoto",
     author_email="fujimoto@phas.ubc.ca",
     description="BNMR/BNQR MUD file reader and asymmetry calculator",
