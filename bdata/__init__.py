61a4d63620a8e5f7c5e28abffa25613bc8a1b041
Derek Fujimoto <fujimoto@phas.ubc.ca>
2019-11-15 19:24:36 -0800

    Added mcomment. Changed default data dir to .mdata

--- bdata/__init__.py
+++ bdata/__init__.py
@@ -1,11 +1,11 @@
 from bdata.bdata import bdata,life
-from bdata.mdata import mdata,mdict,mhist,mscaler,mvar,mcontainer,mlist
+from bdata.mdata import mdata,mdict,mhist,mscaler,mvar,mcontainer,mlist,mcomment
 from bdata.bjoined import bjoined
 from bdata import mudpy
 
 import os
 
 __all__ = ['bdata','mudpy','bjoined','mdata']
-__version__ = '5.0.0'
+__version__ = '5.0.1'
 __author__ = 'Derek Fujimoto'
-_mud_data = os.path.join(os.environ['HOME'],'.bdata')
+_mud_data = os.path.join(os.environ['HOME'],'.mdata')
