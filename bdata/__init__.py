231ae1768d4746625d8de88cb33ee89cb0fa7fe5
Derek Fujimoto <fujimoto@phas.ubc.ca>
2019-12-17 14:22:42 -0800

    v5.0.2

--- bdata/__init__.py
+++ bdata/__init__.py
@@ -6,6 +6,6 @@ from bdata import mudpy
 import os
 
 __all__ = ['bdata','mudpy','bjoined','mdata']
-__version__ = '5.0.1'
+__version__ = '5.0.2'
 __author__ = 'Derek Fujimoto'
 _mud_data = os.path.join(os.environ['HOME'],'.mdata')
