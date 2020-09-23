30189c0e63ca1bf46b50b849fa4597483a90500e
Derek Fujimoto <fujimoto@phas.ubc.ca>
2020-09-23 16:53:28 -0700

    v6.0.2

--- bdata/__init__.py
+++ bdata/__init__.py
@@ -5,6 +5,6 @@ from bdata.bmerged import bmerged
 import os
 
 __all__ = ['bdata','bjoined','bmerged']
-__version__ = '6.0.1'
+__version__ = '6.0.2'
 __author__ = 'Derek Fujimoto'
 _mud_data = os.path.join(os.environ['HOME'],'.bdata')
