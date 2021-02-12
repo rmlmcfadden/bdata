725baa83f3883338a68d3bb1095b9382ba6d697e
Derek Fujimoto <fujimoto@phas.ubc.ca>
2021-02-12 15:46:44 -0800

    v6.2.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bdata",
-    version="6.2.1",
+    version="6.2.2",
     author="Derek Fujimoto",
     author_email="fujimoto@phas.ubc.ca",
     description="BNMR/BNQR MUD file reader and asymmetry calculator",
